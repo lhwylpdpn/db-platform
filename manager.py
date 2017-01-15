4664c2828ea9d216c91395322441f660c532cc83
王志鹏 <zhipeng.wang@corp.elong.com>
2017-01-15 20:30:56 +0800

    850 1200页面迁入
    850 1200页面迁入

--- manager.py
+++ manager.py
@@ -21,6 +21,16 @@ def channelIos600():
     return render_template("channelIos600.html", title="channelIos600")
 
 
+@app.route('/channelIos850')
+def channelIos850():
+    return render_template("channelIos850.html", title="channelIos850")
+
+
+@app.route('/channelIos1200')
+def channelIos1200():
+    return render_template("channelIos1200.html", title="channelIos1200")
+
+
 if __name__ == '__main__':
     # open debug model
     app.debug = True
