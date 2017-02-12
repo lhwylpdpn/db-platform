2b9bf0cfb9c33b7fc7bda617e8b54bd957185669
王志鹏 <zhipeng.wang@corp.elong.com>
2017-02-13 00:49:13 +0800

    登录功能
    登录功能完成；差回车事件和提示错误后光标定位

--- app/controler/power_API.py
+++ app/controler/power_API.py
@@ -1,32 +1,38 @@
 # coding=UTF-8
 import sys
-sys.path.append("..")
-import db.dbBase
-sys.path.append("../..")
-import Config
+from flask import session
+from app.db.dbBase import DBConnect
+from config import Config
 
-conn = db.dbBase.DBConnect().db_connect(Config.Config().DATABASE_MAIN)
-cursor_login = conn.cursor()
-cursor_login_log = conn.cursor()
-def login(username,password):
+
+# moke login function
+def login_in_M(username,password):
+	session['username'] = username
+	result = '{"status":"0"}'
+	return result
+
+
+def login_in(username,password):
 	user = username
 	pwd = password
 	result=""
 	sql="SELECT status,id FROM user_info where username='"+user+"' and password='"+pwd+"' and  status in ('0','1');"
 	#print(sql)
+	conn = DBConnect.db_connect(Config.DATABASE_MAIN)
+	cursor_login = conn.cursor()
 	cursor_login.execute(sql)
 	rs = cursor_login.fetchall()
 	cursor_login.close()
 	if len(rs)>=1:
 		for r in rs:
 			if r[0]=="0":
-
-				result="{'status':'0'}"
+				session['username'] = username
+				result='{"status":"0"}'
 			if r[0]=="1":
-				result="{'status':'1'}"
+				result='{"status":"1"}'
 		login_log(r[1])
 	elif len(rs)<1:
-		result="{'status':‘-1’,'body':'该用户名不存在或密码错误，请重新尝试'}"
+		result='{"status":"-1","body":"该用户名不存在或密码错误，请重新尝试"}'
 		conn.close()
 	
 
@@ -35,6 +41,7 @@ def login(username,password):
 
 def login_log(userid):
 	sql="insert into  login_info values ("+str(userid)+",now())"
+	conn = DBConnect.db_connect(Config.DATABASE_MAIN)
 	cursor_login_log = conn.cursor()
 	cursor_login_log.execute(sql)
 	cursor_login_log.close()
--- app/routes/rUser.py
+++ app/routes/rUser.py
@@ -1,6 +1,6 @@
-from flask import render_template,request
+from flask import render_template, request, Blueprint, session, redirect, url_for
 from app.controler.cUser import CUser
-from flask import Blueprint
+from app.controler.power_API import login_in, login_in_M
 
 userBp = Blueprint('user', __name__, url_prefix="/user")
 
@@ -8,6 +8,20 @@ userBp = Blueprint('user', __name__, url_prefix="/user")
 @userBp.route('/login')
 def login():
     return render_template("user/login.html")
+
+
+@userBp.route('/loginaction')
+def loginaction():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    loginresult = login_in(username, password)
+    return loginresult
+
+
+@userBp.route('/loginout')
+def loginout():
+    session.pop('username', None)
+    return redirect(url_for('index', title='Good Bye'))
 
 
 @userBp.route('/userlist')
