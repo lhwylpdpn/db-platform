0f4cea10c2d0c87a1a0285125b8393e05e52b577
王志鹏 <zhipeng.wang@corp.elong.com>
2017-02-15 00:07:15 +0800

    完成 修改密码功能

--- app/routes/rUser.py
+++ app/routes/rUser.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, Blueprint, session, redirect, url_for
+# This Python file uses the following encoding: utf-8
+from flask import render_template, request, Blueprint, session, redirect, url_for, json
 from app.controler.cUser import CUser
-from app.controler.power_API import login_in, login_in_M
+from app.controler.power_API import login_in, passwd_update
 
 userBp = Blueprint('user', __name__, url_prefix="/user")
 
@@ -27,6 +28,18 @@ def loginout():
 @userBp.route('/modifypassword')
 def modifypassword():
     return render_template("user/modifypassword.html")
+
+
+@userBp.route('/modifypasswordaction')
+def modifypasswordaction():
+    oldpassword = request.args.get('oldpassword')
+    checkResult = login_in(session['username'], oldpassword)  #先需要判断旧密码,用登录方法判断
+    if int(json.loads(checkResult, encoding="utf-8")["status"]) >= 0:
+        newpassword = request.args.get('newpassword')
+        modifypasswordresult = passwd_update(session['username'], newpassword)  #判断通过调用修改密码接口
+        return modifypasswordresult
+    else:
+        return U'{"status":"-1","body":"旧密码错误，请重新尝试。"}'
 
 
 @userBp.route('/cancelMustModPass')
