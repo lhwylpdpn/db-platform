a1e895676821862647b804cb27bbd50f6707405a
王志鹏 <zhipeng.wang@corp.elong.com>
2017-02-15 22:42:37 +0800

    数据权限选择操作用户从数据库读取

--- app/routes/rAuth.py
+++ app/routes/rAuth.py
@@ -1,5 +1,8 @@
-from flask import render_template,request
+# This Python file uses the following encoding: utf-8
+from flask import render_template, request, json
 from flask import Blueprint
+from app.controler.power_API import power_list
+from app.db.dbUser import DBUser
 
 
 authBp = Blueprint('auth', __name__, url_prefix="/auth")
@@ -7,7 +10,12 @@ authBp = Blueprint('auth', __name__, url_prefix="/auth")
 
 @authBp.route('/dataAuth')
 def dataAuth():
-    return render_template("authority/dataAuth.html")
+    # userlistt = json.loads(power_list(""), encoding="utf-8")  # 字符串传化为json 对象
+    # userlistt = tuple(userlistt["body"])  # 将json对象的body转化为列表(转化为列表只会保留key)
+    # print(userlistt)
+    userlist = DBUser.userLists()
+    print( userlist )
+    return render_template("authority/dataAuth.html", userlist=userlist)
 
 
 @authBp.route('/menuAuth')
