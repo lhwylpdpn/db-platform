1d0190fb69ac2a335d64a4a689108af1fd829c29
王志鹏 <zhipeng.wang@corp.elong.com>
2017-03-04 22:07:04 +0800

    提交 mediaOverview

--- app/routes/rAnalyze.py
+++ app/routes/rAnalyze.py
@@ -1,5 +1,7 @@
 # This Python file uses the following encoding: utf-8
 from flask import render_template, request, Blueprint, session, redirect, url_for, json
+import random
+
 from app.controler.cUser import CUser
 from app.controler.power_API import login_in, passwd_update
 
@@ -8,7 +10,7 @@ analyzeBp = Blueprint('analyze', __name__, url_prefix="/analyze")
 
 @analyzeBp.route('/mediaOverview')
 def mediaOverview():
-    return render_template("putAnalyze/mediaAnalyze/mediaOverview.html", title=U"媒体概览")
+    return render_template("putAnalyze/mediaAnalyze/mediaOverview.html", title=U"媒体概览", sjs=random.random())
 
 
 
