550366cd456cd18ccaaf278263e91d4b2dde6377
ppp <zhipeng.wang@corp.elong.com>
2017-03-08 22:14:27 +0800

    提交灌数和尝试日期filter

--- app/routes/rAnalyze.py
+++ app/routes/rAnalyze.py
@@ -2,8 +2,8 @@
 from flask import render_template, request, Blueprint, session, redirect, url_for, json
 import random
 
-from app.controler.cUser import CUser
-from app.controler.power_API import login_in, passwd_update
+from app.controler.power_API import get_business_json
+
 
 analyzeBp = Blueprint('analyze', __name__, url_prefix="/analyze")
 
@@ -13,4 +13,7 @@ def mediaOverview():
     return render_template("putAnalyze/mediaAnalyze/mediaOverview.html", title=U"媒体概览", sjs=random.random())
 
 
-
+@analyzeBp.route('/mediaOverviewJson')
+def mediaOverviewJson():
+    jsons = json.loads(get_business_json("media_1.json", session["username"]))  # 字符串传化为json 对象
+    return json.dumps(jsons["body"])
