6bdfc832b737d437072fbcf03bc55fc227aba0d6
liuhao <58254451@qq.com>
2017-04-20 19:47:26 +0800

    写吐了，上半部分筛选没问题， 下面返回的时候有错。明天再调整

--- app/routes/rAnalyze.py
+++ app/routes/rAnalyze.py
@@ -1,9 +1,9 @@
 # This Python file uses the following encoding: utf-8
 from flask import render_template, request, Blueprint, session, redirect, url_for, json
 import random
-
+import re
 from app.controler.power_API import get_business_json
-
+import time
 
 analyzeBp = Blueprint('analyze', __name__, url_prefix="/analyze")
 
@@ -59,7 +59,7 @@ def mediaOverviewJson():
         jsons_filter_staffs = jsons_filter_agents
 
     # 按照日期 group by 筛选出有的日期
-    newjson = []
+    newjson= []
     for ii in range(len(jsons_filter_staffs)):
         flag = 1
         for jj in range(len(newjson)):
@@ -144,7 +144,48 @@ def mediaRetention():
 
 @analyzeBp.route('/mediaRetentionJson')
 def mediaRetentionJson():
-    jsons = json.loads(get_business_json("media_3.json", session["username"]))  # 字符串传化为json 对象 dict
-    print(jsons)
+    args=request.args.items()
+    jsons = json.loads(get_business_json("media_3.json", session["username"]))["body"]
+    jsons = json.dumps(jsons)
+    jsons = jsons.replace(" ","")
+    word=""
+    resultjson=""
+    for x in xrange(0,len(args)):
+        if args[x][1]!="":
+            word=r'({[^}]*"'+args[x][0]+'":"'+args[x][1].replace(',','"[^}]+}|{[^}]+"'+args[x][0]+'":"')+'"[^}]*})'
+            jsons=",".join(re.findall(word,jsons))
+            print(4)
+        else:
+            word=r'({[^}]*[^}]*})'
+            jsons=",".join(re.findall(word,jsons))
 
-    #r=re.findall(r'({[^}]+"channel_name":"百度品牌专区"[^}]+}|{[^}]+"agent":"晋拓"[^}]+})',str1)
+    jsons=json.loads("["+jsons+"]")
+
+    new_json=[]
+    date_temp=[]
+    print time.asctime(time.localtime(time.time()))
+    jsons.sort(key=lambda jsons:jsons.get("date_",0))
+    for x in xrange(1,len(jsons)):
+        if jsons[x]["date_"] == jsons[x-1]["date_"]:
+            if jsons[x-1]["re_0"]=="":
+
+                jsons[x]["re_0"]=int(round(float(jsons[x]["re_0"])))+int(round(float(jsons[x-1]["re_0"]))) if len(jsons[x-1]["re_0"])>0 else int(round(float(jsons[x]["re_0"])))
+                jsons[x]["re_1"]=int(round(float(jsons[x]["re_1"])))+int(round(float(jsons[x-1]["re_1"]))) if len(jsons[x-1]["re_1"])>0 else int(round(float(jsons[x]["re_1"])))
+                jsons[x]["re_2"]=int(round(float(jsons[x]["re_2"])))+int(round(float(jsons[x-1]["re_2"]))) if len(jsons[x-1]["re_2"])>0 else int(round(float(jsons[x]["re_2"])))
+                jsons[x]["re_3"]=int(round(float(jsons[x]["re_3"])))+int(round(float(jsons[x-1]["re_3"]))) if len(jsons[x-1]["re_3"])>0 else int(round(float(jsons[x]["re_3"])))
+                jsons[x]["re_4"]=int(round(float(jsons[x]["re_4"])))+int(round(float(jsons[x-1]["re_4"]))) if len(jsons[x-1]["re_4"])>0 else int(round(float(jsons[x]["re_4"])))
+                jsons[x]["re_5"]=int(round(float(jsons[x]["re_5"])))+int(round(float(jsons[x-1]["re_5"]))) if len(jsons[x-1]["re_5"])>0 else int(round(float(jsons[x]["re_5"])))
+                jsons[x]["re_6"]=int(round(float(jsons[x]["re_6"])))+int(round(float(jsons[x-1]["re_6"]))) if len(jsons[x-1]["re_6"])>0 else int(round(float(jsons[x]["re_6"])))
+                jsons[x-1]=""
+    
+    for x in xrange(0,len(jsons)):
+        if jsons[x]!="":
+            resultjson+=jsons[x]+","
+        resultjson=resultjson[0:-1]
+    print time.asctime(time.localtime(time.time()))
+    r="["+resultjson+"]"
+    return r
+
+
+
+
