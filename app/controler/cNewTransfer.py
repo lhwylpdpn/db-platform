a6e27aed7e82929e08362bc70e8ceea100871eb6
liuhao <58254451@qq.com>
2017-04-24 19:00:54 +0800

    更新查询条件

--- app/controler/cNewTransfer.py
+++ app/controler/cNewTransfer.py
@@ -9,9 +9,50 @@ import time
 class cNewTransfer:
 	@staticmethod
 	def newTransferTJ(username):
-		jsons = json.loads(get_business_json("media_2.json", username))  # 字符串传化为json 对象
-		return json.dumps(jsons["body"])
+		game_id=[]
+		platform=[]
+		channel_name=[]
+		staff=[]
+		agent=[]
+		tj=""
+		jsons = json.loads(get_business_json("media_2.json", username))["body"]  # 字符串传化为json 对象
 
+		for x in xrange(0,len(jsons)):
+			game_id.append(jsons[x]["game_id"])
+			platform.append(jsons[x]["platform"])
+			channel_name.append(jsons[x]['channel_name'])
+			agent.append(jsons[x]['agent'])
+			staff.append(jsons[x]['staff'])
+		game_id=list(set(game_id))
+		platform=list(set(platform))
+		channel_name=list(set(channel_name))
+		agent=list(set(agent))
+		staff=list(set(staff))
+		tj+='{"game_id":['
+		for x in xrange(0,len(game_id)):
+			tj+='{"id":"'+str(game_id[x])+'","text":"'+str(game_id[x])+'"},'
+		tj=tj[0:-1]+'],'
+		tj+='"channel_name":['
+		for x in xrange(0,len(channel_name)):
+			tj+='{"id":"'+str(channel_name[x].encode('utf-8'))+'","text":"'+str(channel_name[x].encode('utf-8'))+'"},'
+		tj=tj[0:-1]+'],'
+		tj+='"staff":['
+		for x in xrange(0,len(staff)):
+			tj+='{"id":"'+str(staff[x].encode('utf-8'))+'","text":"'+str(staff[x].encode('utf-8'))+'"},'
+		tj=tj[0:-1]+'],'
+		tj+='"platform":['
+		for x in xrange(0,len(platform)):
+			tj+='{"id":"'+str(platform[x].encode('utf-8'))+'","text":"'+str(platform[x].encode('utf-8'))+'"},'
+		tj=tj[0:-1]+'],'
+		tj+='"agent":['
+		for x in xrange(0,len(agent)):
+			tj+='{"id":"'+str(agent[x].encode('utf-8'))+'","text":"'+str(agent[x].encode('utf-8'))+'"},'
+		tj=tj[0:-1]
+
+
+		tj=tj+']}'
+		
+		return tj
 	@staticmethod
 	def newTransferJson(username):
 		jsons = json.loads(get_business_json("media_2.json", username))["body"]  # 字符串传化为json 对象 dict
