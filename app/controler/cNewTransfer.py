59735b6f535637f8611b11c5430e488ddfa65817
ppp <zhipeng.wang@corp.elong.com>
2017-04-26 22:31:20 +0800

    先提交了

--- app/controler/cNewTransfer.py
+++ app/controler/cNewTransfer.py
@@ -59,115 +59,75 @@ class cNewTransfer:
 		return json.dumps(jsons)
 
 	@staticmethod
-	def newTransferJson(username, filterS):
-		# @liuhao filterS 传过来了
-		jsons = json.loads(get_business_json("media_2.json", username))["body"]  # 字符串传化为json 对象 dict
+	def newTransferJson(username, args):
+		# args = request.args.items()  # 获取请求list
+		jsons = json.loads(get_business_json("media_2.json", username).encode("utf-8"))["body"]  # 获取body内容，注意为了匹配要encode一下 utf8
+		jsons = json.dumps(jsons, ensure_ascii=False)  # 转成字符串，必须要有ensure_ascii=False，不然会自动转成ascii
+		jsons = jsons.replace(" ", "")  # 转完的 键值对之间有空格要去掉
+		resultjson = []
+		word = r'({[^}]*[^}]*})'  # 将jsons字符串 筛选成{},{}的格式，也为了应对args根本没有参数，直接可以返回个全的
+		jsons = ",".join(re.findall(word, jsons))  # 将筛选的结果拼成list，然后用，连接成字符串
+		for x in xrange(0, len(args)):  # 循环args 将所有参数拼接成正则表达式word，并进行筛选，诗选结果拼成一个list
+			if args[x][0] == "date_" or args[x][0] == "channel_name" or args[x][0] == "game_id" or args[x][
+				0] == "platform" or args[x][0] == "agent" or args[x][0] == "staff":
+				if args[x][1] != "":
+					word = r'({[^}]*"' + args[x][0] + '":"' + args[x][1].replace(',', '"[^}]+}|{[^}]+"' + args[x][
+						0] + '":"') + '"[^}]*})'
 
-		# 处理channel请求
-		channel_name = request.args.get('channel_name')
-		jsons_filter_channel_names = []
-		if channel_name != "":
-			channel_names = channel_name.split(",")
-			for i in range(len(jsons)):
-				if jsons[i]["channel_name"] in channel_names:
-					jsons_filter_channel_names.append(jsons[i])
-		else:
-			jsons_filter_channel_names = jsons
+					jsons = ",".join(re.findall(word, jsons))
 
-		# 处理agent请求
-		agent = request.args.get('agent')
-		jsons_filter_agents = []
-		if agent != "":
-			agents = agent.split(",")
-			for i in range(len(jsons_filter_channel_names)):
-				if jsons_filter_channel_names[i]["agent"] in agents:
-					jsons_filter_agents.append(jsons_filter_channel_names[i])
-		else:
-			jsons_filter_agents = jsons_filter_channel_names
-
-		# 处理staff请求
-		staff = request.args.get('staff')
-		jsons_filter_staffs = []
-		if staff != "":
-			staffs = staff.split(",")
-			for i in range(len(jsons_filter_agents)):
-				if jsons_filter_agents[i]["staff"] in staffs:
-					jsons_filter_staffs.append(jsons_filter_agents[i])
-		else:
-			jsons_filter_staffs = jsons_filter_agents
-
-		# 按照日期 group by 筛选出有的日期
-		newjson = []
-		for ii in range(len(jsons_filter_staffs)):
-			flag = 1
-			for jj in range(len(newjson)):
-				if newjson != []:
-					if jsons_filter_staffs[ii]["date_time"] == newjson[jj]["date_time"]:
-						flag = 0
-						break
 				else:
-					flag = 1
-			if flag:
-				newjson.append({"date_time": jsons_filter_staffs[ii]["date_time"]})
+					word = r'({[^}]*[^}]*})'
+					jsons = ",".join(re.findall(word, jsons))
 
-		# 拼凑json串
-		for ii in range(len(newjson)):
-			newjson[ii]["channel_name"] = "groupby"  # 渠道
-			newjson[ii]["agent"] = "groupby"  # 代理
-			newjson[ii]["staff"] = "groupby"  # 负责人
-			newjson[ii]["ad_click"] = 0.0  # 点击设备
-			newjson[ii]["ad_action"] = 0.0  # 激活设备
-			newjson[ii]["ad_action_new"] = 0.0  # 新增设备
-			newjson[ii]["ad_account_new"] = 0.0  # 新增总账号
-			newjson[ii]["double_new"] = 0.0  # 纯新增账号
-			newjson[ii]["back_user"] = 0.0  # 回流账号
-			newjson[ii]["paid_account"] = 0.0  # 付费账号
-			newjson[ii]["spend"] = 0.0  # 折后花费
-			newjson[ii]["cumulative_flow"] = 0.0  # 累计流水
-			newjson[ii]["dis"] = 0.0  # 分成比例
-			newjson[ii]["cumulative_moeny"] = 0.0  # 累计净收入
+		jsons = jsons.replace('"re_0":""', '"re_0":0')
+		jsons = jsons.replace('"re_1":""', '"re_1":0')
+		jsons = jsons.replace('"re_2":""', '"re_2":0')
+		jsons = jsons.replace('"re_3":""', '"re_3":0')
+		jsons = jsons.replace('"re_4":""', '"re_4":0')
+		jsons = jsons.replace('"re_5":""', '"re_5":0')
+		jsons = jsons.replace('"re_6":""', '"re_6":0')  # 空变成0，方便我后面计算
+		jsons = json.loads("[" + jsons + "]")  # 加上[]  就可以返回了
+		new_json = []
+		date_temp = []
+		print(5)
+		print time.asctime(time.localtime(time.time()))
+		jsons.sort(key=lambda jsons: jsons.get("date_", 0))
+		print(6)
+		for x in xrange(1, len(jsons)):
+			if jsons[x]["date_"] == jsons[x - 1]["date_"]:
 
-		# 讲数据累加带对应日期
-		for jj in jsons_filter_staffs:
-			for ii in range(len(newjson)):
-				if jj["date_time"] == newjson[ii]["date_time"]:
-					newjson[ii]["ad_click"] = newjson[ii]["ad_click"] + int(jj["ad_click"])  # 点击设备
-					newjson[ii]["ad_action"] = newjson[ii]["ad_action"] + int(jj["ad_action"])  # 激活设备
-					newjson[ii]["ad_action_new"] = newjson[ii]["ad_action_new"] + int(jj["ad_action_new"])  # 新增设备
-					newjson[ii]["ad_account_new"] = newjson[ii]["ad_account_new"] + int(jj["ad_account_new"])  # 新增总账号
-					newjson[ii]["double_new"] = newjson[ii]["double_new"] + int(jj["double_new"])  # 纯新增账号
-					newjson[ii]["back_user"] = newjson[ii]["back_user"] + int(jj["back_user"])  # 回流账号
-					newjson[ii]["paid_account"] = newjson[ii]["paid_account"] + int(jj["paid_account"])  # 付费账号
-					newjson[ii]["spend"] = newjson[ii]["spend"] + float(jj["spend"])  # 折后花费
-					newjson[ii]["cumulative_flow"] = newjson[ii]["cumulative_flow"] + float(
-						jj["cumulative_flow"])  # 累计流水
-					newjson[ii]["dis"] = newjson[ii]["dis"] + float(jj["dis"])  # 分成比例
-					newjson[ii]["cumulative_moeny"] = newjson[ii]["cumulative_moeny"] + float(
-						jj["cumulative_moeny"])  # 累计净收入
-
-		# 计算 fufeilv CPA ROI
-		for ii in range(len(newjson)):
-			if float(newjson[ii]["ad_account_new"]) != 0:
-				newjson[ii]["fufeilv"] = newjson[ii]["paid_account"] / float(newjson[ii]["ad_account_new"])  # fufeilv
-				newjson[ii]["CPA"] = newjson[ii]["spend"] / float(newjson[ii]["ad_account_new"])  # CPA
-
-				newjson[ii]["fufeilv"] = round(newjson[ii]["fufeilv"], 2)
-				newjson[ii]["CPA"] = round(newjson[ii]["CPA"], 2)
-
+				jsons[x]["re_0"] = int(round(float(jsons[x]["re_0"]))) + int(round(float(jsons[x - 1]["re_0"])))
+				jsons[x]["re_1"] = int(round(float(jsons[x]["re_1"]))) + int(round(float(jsons[x - 1]["re_1"])))
+				jsons[x]["re_2"] = int(round(float(jsons[x]["re_2"]))) + int(round(float(jsons[x - 1]["re_2"])))
+				jsons[x]["re_3"] = int(round(float(jsons[x]["re_3"]))) + int(round(float(jsons[x - 1]["re_3"])))
+				jsons[x]["re_4"] = int(round(float(jsons[x]["re_4"]))) + int(round(float(jsons[x - 1]["re_4"])))
+				jsons[x]["re_5"] = int(round(float(jsons[x]["re_5"]))) + int(round(float(jsons[x - 1]["re_5"])))
+				jsons[x]["re_6"] = int(round(float(jsons[x]["re_6"]))) + int(round(float(jsons[x - 1]["re_6"])))
+				jsons[x - 1] = ""
 			else:
-				newjson[ii]["fufeilv"] = '0'
-				newjson[ii]["CPA"] = '0'
+				jsons[x]["re_0"] = int(round(float(jsons[x]["re_0"])))
+				jsons[x]["re_1"] = int(round(float(jsons[x]["re_1"])))
+				jsons[x]["re_2"] = int(round(float(jsons[x]["re_2"])))
+				jsons[x]["re_3"] = int(round(float(jsons[x]["re_3"])))
+				jsons[x]["re_4"] = int(round(float(jsons[x]["re_4"])))
+				jsons[x]["re_5"] = int(round(float(jsons[x]["re_5"])))
+				jsons[x]["re_6"] = int(round(float(jsons[x]["re_6"])))
 
-			if float(newjson[ii]["spend"]) != 0:
-				newjson[ii]["ROI"] = newjson[ii]["cumulative_moeny"] / float(newjson[ii]["spend"])  # ROI
-				newjson[ii]["ROI"] = round(newjson[ii]["ROI"] * 100, 2)
-			else:
-				newjson[ii]["ROI"] = '0'
+		for item in jsons[:]:
+			if item == "":
+				jsons.remove(item)
 
-			# 将float 保留2位小数
-			newjson[ii]["spend"] = round(newjson[ii]["spend"], 2)
-			newjson[ii]["cumulative_flow"] = round(newjson[ii]["cumulative_flow"], 2)
-			newjson[ii]["dis"] = round(newjson[ii]["dis"], 2)
-			newjson[ii]["cumulative_moeny"] = round(newjson[ii]["cumulative_moeny"], 2)
+			# resultjson=resultjson[0:-1]
+		print time.asctime(time.localtime(time.time()))
+		jsons = json.dumps(jsons)
 
-		return json.dumps(newjson)  # 将dict 转化成 字符串
+		jsons = jsons.replace(" ", "")
+		jsons = jsons.replace('"re_0":0', '"re_0":""')
+		jsons = jsons.replace('"re_1":0', '"re_1":""')
+		jsons = jsons.replace('"re_2":0', '"re_2":""')
+		jsons = jsons.replace('"re_3":0', '"re_3":""')
+		jsons = jsons.replace('"re_4":0', '"re_4":""')
+		jsons = jsons.replace('"re_5":0', '"re_5":""')
+		jsons = jsons.replace('"re_6":0', '"re_6":""')
+		return jsons
