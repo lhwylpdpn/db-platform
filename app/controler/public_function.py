5c86f0f79538a4b95ae29be181805e2768e8a171
liuhao <58254451@qq.com>
2017-05-02 16:35:09 +0800

    查询条件函数挪到public_function

--- app/controler/public_function.py
+++ app/controler/public_function.py
@@ -3,8 +3,8 @@ from flask import render_template, request, Blueprint, session, redirect, url_fo
 import random
 import re
 import time
-
-
+from app.controler.power_API import get_business_json
+import datetime
 def get_first_letter(char):
    char=char.encode('GBK')
    
@@ -56,3 +56,74 @@ def get_first_letter(char):
        return "y"
    if char<b"\xd7\xf9":
        return "z"
+
+def p_analyzeTJ(username,filternode):
+
+    game_id=[]
+    platform=[]
+    channel_name=[]
+    staff=[]
+    agent=[]
+    tj=""
+    tjtype=""
+    args = filternode
+
+    jsons = json.loads(get_business_json("media_TJ.json", username))["body"]
+
+    jsons = json.dumps(jsons,ensure_ascii=False)
+    jsons = jsons.replace(" ","")
+
+    resultjson=[]
+    word=r'({[^}]*[^}]*})'
+    jsons=",".join(re.findall(word,jsons))
+
+    for x in xrange(0,len(args)):
+      if args[x][0]=="tjtype":
+        tjtype=args[x][1]
+      if args[x][0]=="channel_name"   or args[x][0]=="agent" or args[x][0]=="staff" :
+        if args[x][1]!="":
+          word=r'({[^}]*"'+args[x][0]+'":"'+args[x][1].replace(',','"[^}]+}|{[^}]+"'+args[x][0]+'":"')+'"[^}]*})'
+  
+          jsons=",".join(re.findall(word,jsons))
+       
+        else:
+          word=r'({[^}]*[^}]*})'
+          jsons=",".join(re.findall(word,jsons))
+
+    jsons=json.loads("["+jsons+"]")
+
+
+    for x in xrange(0,len(jsons)):
+      #game_id.append(jsons[x]["game_id"])
+      #platform.append(jsons[x]["platform"])
+      channel_name.append(jsons[x]['channel_name'])
+      agent.append(jsons[x]['agent'])
+      staff.append(jsons[x]['staff'])
+    #game_id=list(set(game_id))
+    #platform=list(set(platform))
+    channel_name=list(set(channel_name))
+    agent=list(set(agent))
+    staff=list(set(staff))
+    tj="["
+
+    if tjtype=="channel_name":
+      for x in xrange(0,len(channel_name)):
+
+        tj+='{"channel_name":"'+str(channel_name[x])+'","initial":"'+str(get_first_letter(channel_name[x]))+'"},'
+      tj=tj[0:-1]
+    if tjtype=="agent":
+      for x in xrange(0,len(agent)):
+        tj+='{"agent":"'+str(agent[x])+'","initial":"'+str(get_first_letter(agent[x]))+'"},'
+      tj=tj[0:-1]
+    if tjtype=="staff":
+      for x in xrange(0,len(staff)):
+        tj+='{"staff":"'+str(staff[x])+'","initial":"'+str(get_first_letter(staff[x]))+'"},'
+      tj=tj[0:-1]
+    # if tjtype=="platform":
+    #   for x in xrange(0,len(platform)):
+    #     tj+='{"platform":"'+str(platform[x])+'","initial":"'+str(get_first_letter(platform[x]))+'"},'
+    #   tj=tj[0:-1]
+    tj=tj+']'
+    
+
+    return tj
