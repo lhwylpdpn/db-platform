a1f5381a383511d8c52385504c28940436b67417
liuhao <58254451@qq.com>
2017-05-17 13:58:22 +0800

    格式问题

--- app/controler/service_manager.py
+++ app/controler/service_manager.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
 		csv_import_A.import_csv("/data1/bidata/1452827692979/","market_logout_log_"+str((datetime.datetime.now()+datetime.timedelta(days=-1)).strftime('%Y-%m-%d'))+".csv",'logout_detail','1')
 		csv_import_A.import_csv("/data1/bidata/1452827692979/","market_recharge_log_"+str((datetime.datetime.now()+datetime.timedelta(days=-1)).strftime('%Y-%m-%d'))+".csv",'recharge_detail','1')
 
-		csv_import_A.import_clac():#补充自然量的数据到spend表
+		csv_import_A.import_clac()#补充自然量的数据到spend表
 
 		print("step3 ok")
 		media_export_json.export_meida_all()
